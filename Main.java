028b8694ff1f04c7b5ec9b590348989d51a4d42b
Matthew Lavine <hermitlavine@gmail.com>
2013-03-20 21:36:13 -0400

    Began to make messenger accept strings.

--- Main.java
+++ Main.java
@@ -3,22 +3,14 @@ import java.lang.String;
 
 class Main {
 	public static void main(String[] args) {
-		int num = 0;
+		String msg = "";
 		int hash = 0;
 		Scanner input = new Scanner (System.in);
 		
 		if (args[0].equals("e")) {
-			System.out.print("Enter number to encrypt: ");		
-			num = Integer.parseInt(input.next());
-			hash = num*52;
-			System.out.println("You're hash is: " + hash);
-		}	
-		
-		if (args[0].equals("d")) {
-			System.out.print("Enter hash to decrypt: ");
-			hash = Integer.parseInt(input.next());
-			num = hash/52;
-			System.out.println("The number is: " + num);
+			System.out.print("Enter a message to encrypt: ");		
+			msg = input.next();
+			System.out.print(msg);		
 		}	
 	}
 }

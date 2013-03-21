7a57def08fe18e4732a4644a433309613fc89b14
Matthew Lavine <hermitlavine@gmail.com>
2013-03-20 22:22:59 -0400

    Added Calling File

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void Main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		encryptHash encrypt = new encryptHash();
+		System.out.println("Please enter a message: ");
+		encrypt.encryptHash("matt", scan.nextLine());
+	}
+}
